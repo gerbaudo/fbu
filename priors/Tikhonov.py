cb66527e38a5bd0cc8fe98d771c6d6125bf64d63
clementhelsens <clement.helsens@gmail.com>
2013-10-11 14:44:25 +0200

    add a name (tikhonov) for the stochastic variable

--- priors/Tikhonov.py
+++ priors/Tikhonov.py
@@ -3,7 +3,7 @@ import numpy
 from math import fabs
 
 def Tikhonov_factory(nreco=4, refcurv=6.1e05,alpha=1e-8, t_l=10, t_h=1000):
-    @pymc.stochastic
+    @pymc.stochastic(name='tikhonov')
     def truth(value=[(t_l+t_h)/2 for xx in xrange(nreco)],refcurv=refcurv, alpha=alpha, low=t_l, high=t_h):
 
         for val in value:
@@ -14,6 +14,11 @@ def Tikhonov_factory(nreco=4, refcurv=6.1e05,alpha=1e-8, t_l=10, t_h=1000):
         curvature = sum([c*c for c in map(computeCurvature,range(1,nreco-1))])
 
         deltaCurv = fabs(curvature-refcurv)
+
         return -deltaCurv*alpha
 
+
+
     return truth
+
+
