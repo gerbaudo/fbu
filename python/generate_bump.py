dbe9c3157aa2db4a2795cbe0be9eabcd4f30a11d
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-10-13 04:38:10 -0700

    still work in progress, getting there

--- python/generate_bump.py
+++ python/generate_bump.py
@@ -8,7 +8,11 @@ import os
 from utils import array2json, json2array
 from pyFBU import pyFBU
 
+nBinsTruth, truthMin, truthMax = 12, 500.0, 4500.0
+nBinsReco,  recoMin,  recoMax  = 12, 500.0, 4500.0
+
 def generateTruthSpectrum(offset=100.0, scale=500.0, nbkg=1e6, nsig=1e5) :
+# def generateTruthSpectrum(offset=100.0, scale=500.0, nbkg=10, nsig=2) :
     """Generate pseudo-events with an observable in [offset, scale].
     The observable spectrum is inspired to the one expected for a
     di-jet resonance, where the background is distributed as a falling
@@ -33,6 +37,7 @@ jsonSmearFname  = jsonDir+'reco.json'
 jsonResMatFname = jsonDir+'resMat.json'
 
 regenerate = not os.path.exists(jsonResMatFname)
+regenerate = True
 
 def normalized(mat) :
     mat = mat.astype(float)
@@ -41,15 +46,36 @@ def normalized(mat) :
 def generateAndPlot() :
     sigPlusBkg = generateTruthSpectrum()
     spbSmeared = np.array([smear(x) for x in sigPlusBkg])
-    numBins = 50
-    nBinsTruth, truthMin, truthMax = 50, 500.0, 4500.0
-    nBinsReco,  recoMin,  recoMax  = 50, 500.0, 4500.0
     histTruth, binsTruth        = np.histogram(sigPlusBkg, bins=nBinsTruth, range=(truthMin, truthMax))
     histReco,  binsReco         = np.histogram(spbSmeared, bins=nBinsReco, range=(recoMin, recoMax))
     respHist,  xedges, yedges   = np.histogram2d(sigPlusBkg, spbSmeared, bins=(nBinsTruth, nBinsReco),
                                                  range=((truthMin, truthMax), (recoMin,  recoMax)))
-    respMat = normalized(respHist)
-    respMat = np.flipud(respMat) # histogram2d returns the y axis reversed (see docs)
+    print 'in truth histo ',histTruth.shape,': ',histTruth
+    print 'in reco histo  ',histReco.shape ,': ',histReco
+    print 'in respHist    ',respHist.shape ,': ',respHist
+    def prMatrix(recoVsTruthCounts) :
+        """
+        This matrix is p(r|t). Given recoVsTruthCounts, where truth is
+        on axis=0 and reco is on axis=1, we can just obtain the p(r|t)
+        matrix by normalizing each bin [t][r] by the total N of evts
+        in that 'row' [t]:
+
+           [t][r] -> [t][r]/sum([[t][r] for r in nreco])
+
+        With np this can be done with the broadcasting mechanims. Try:
+        a = np.array([[1., 2.],
+                      [3., 4.]])
+        a / np.sum(a, axis=1)[:,np.newaxis]
+        --> [[ 0.33333333,  0.66666667],
+             [ 0.42857143,  0.57142857]]
+        And see for example
+        http://stackoverflow.com/questions/7140738/numpy-divide-along-axis)
+        """
+        return recoVsTruthCounts / np.sum(recoVsTruthCounts, axis=1)[:,np.newaxis]
+#return recoVsTruthCounts / truthCounts.astype('float')[:,np.newaxis]
+    respMat = prMatrix(respHist)
+    print 'respMat :'
+    print respMat
     def plotTruthAndReco(histT, binsT, histR, binsR, outfname) :
         def getWidth(bins) : return bins[1] - bins[0]
         plt.figure()
@@ -80,8 +106,11 @@ else :
     histReco   = json2array(jsonSmearFname),
     respMat    = json2array(jsonResMatFname)
 
+print 'histTruth : ',histTruth
+print 'histReco : ',histReco
+print 'respMat : ',respMat
 pyfbu = pyFBU()
-pyfbu.nMCMC        = 1000
+pyfbu.nMCMC        = 100000
 pyfbu.nBurn        = 100
 pyfbu.nThin        = 10
 pyfbu.lower        = 1
@@ -93,3 +122,19 @@ pyfbu.verbose      = True # verbose ? will be on cmd-line
 pyfbu.run()
 
 trace = pyfbu.trace
+print trace
+estMean   = np.mean(trace, axis=0)
+estMedian = np.median(trace, axis=0)
+estStd    = np.std(trace, axis=0)
+print 'estMean: ',estMean
+print 'estMedian: ',estMedian
+xdata = np.linspace(truthMin, truthMax, nBinsTruth)
+binW = (truthMax-truthMin)/nBinsTruth
+plt.figure()
+print 'xdata ',len(xdata)
+print 'histTruth ',len(histTruth)
+plt.bar(xdata, histTruth, color='b', width=binW, alpha=0.15)
+#plt.bar(xdata, histReco, color='r',  width=binW, alpha=0.15)
+
+plt.errorbar(xdata+0.5*binW, estMedian, yerr=estStd, fmt='k.')
+plt.savefig('foo.png')
