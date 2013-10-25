095c707787e2fd9b9e6d565d7da37b9e524c0ed4
Francesco Rubbo <francescorubbo@pb-d-128-141-154-45.cern.ch>
2013-10-25 15:16:29 +0200

    include LICENSE in package and read it in setup.py

---
+++ fbu/__init__.py
@@ -0,0 +1,1 @@
+#fbu package
--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 
 setup(
     name='fbu',
-    version='0.0.0dev',
+    version='0.0.1dev.2',
     packages=['fbu',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='LICENSE.txt',
     long_description=open('README.txt').read(),
+    url='https://pypi.python.org/pypi/fbu',
+    author='Davide Gerbaudo,Clement Helsens,Francesco Rubbo',
+    author_email='rubbo.francesco@gmail.com',
     )
     
