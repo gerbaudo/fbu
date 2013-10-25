1bb32ea9e56810f1482b2992378dd99de2450cc6
Francesco Rubbo <francescorubbo@pb-d-128-141-154-45.cern.ch>
2013-10-25 12:52:14 +0200

    use setup.py for versioning

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='fbu',
+    version='0.0.0dev',
+    packages=['fbu',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    )
+    
