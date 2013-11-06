87769d449f03ff703d40261ec22e731e1bb94e61
francescorubbo <rubbo.francesco@gmail.com>
2013-11-06 16:59:44 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fbu',
-    version='0.0.1dev.2',
+    version='0.0.1dev.4',
     packages=['fbu',],
     license='LICENSE.txt',
     long_description=open('README.txt').read(),
