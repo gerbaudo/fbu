7c79f098a76069ccde675b03f598c8e2e816bbd2
francescorubbo <rubbo.francesco@gmail.com>
2013-11-17 21:49:42 +0100

    use lower and upper as a list

--- fbu/validation.py
+++ fbu/validation.py
@@ -2,7 +2,7 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from numpy import mean,std,arange
 
-def plot(dirname,data,bkgd,resmat,trace,lower=0,upper=0):
+def plot(dirname,data,bkgd,resmat,trace,lower=[],upper=[]):
     import os
     if not os.path.exists(dirname+'_monitoring'):
         os.makedirs(dirname+'_monitoring')
@@ -19,7 +19,7 @@ def plot(dirname,data,bkgd,resmat,trace,lower=0,upper=0):
         plt.ylabel('Probability')
         plt.xlabel('Bin content')
         ymean = mean(ax.get_ylim())
-        plt.hlines(ymean,lower,upper,linestyles='dashed',colors='m',label='hyperbox')
+        plt.hlines(ymean,lower[bin],upper[bin],linestyles='dashed',colors='m',label='hyperbox')
         plt.vlines(data[bin],0.,ymean,linestyles='solid',colors='c',label='data')
         plt.xlim(xmin=0)
         plt.subplot(212)
