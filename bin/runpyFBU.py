06308446daf4c4958b64d012d0ffad28c5096995
francescorubbo <rubbo.francesco@gmail.com>
2013-11-19 15:26:42 +0100

    run on A2pos and turn off second iteration with narrow hyperbox

--- bin/runpyFBU.py
+++ bin/runpyFBU.py
@@ -10,7 +10,7 @@ from optparse import OptionParser
 if __name__ == "__main__":
     projectdir = os.path.dirname(os.path.abspath(__file__)).replace('/bin','/')
     datadir = projectdir+'tests/linearity/data/mc11/'
-    defaultdata = datadir+'data.json'
+    defaultdata = datadir+'dataA2pos.json'
     defaultmig  = datadir+'migrations.json'
     defaultbkg  = datadir+'background.json'
     usage = "usage: %prog -t python/unfold_template.py [options]"
@@ -39,21 +39,25 @@ if __name__ == "__main__":
     pyfbu.upper            = [140000]*ndim
     pyfbu.response         = json.load(open(jsonmig))
     pyfbu.background       = json.load(open(jsonbkg))
-    pyfbu.backgroundsyst = {'bckg':0.0}
+    pyfbu.backgroundsyst = {'bckg':0.1}
     pyfbu.rndseed          = int(rndseed)
     pyfbu.verbose          = verbose
     pyfbu.monitoring = True
     pyfbu.name = 'test'
     pyfbu.run()
 
-    trace = pyfbu.trace
-    from numpy import mean,std
-    mm = mean(trace,1)
-    ss = std(trace,1)
-
-    pyfbu.lower            = mm-5*ss
-    pyfbu.upper            = mm+5*ss
-
-    pyfbu.name = 'optim'
-    pyfbu.backgroundsyst = {'bckg':0.1}
-    pyfbu.run()
+    if False:
+        trace = pyfbu.trace
+        from numpy import mean,std
+        mm = mean(trace,1)
+        ss = std(trace,1)
+        
+        pyfbu.lower            = mm-5*ss
+        pyfbu.upper            = mm+5*ss
+        
+        pyfbu.name = 'optim'
+        pyfbu.backgroundsyst = {'bckg':0.1}
+        pyfbu.nMCMC            = 500000
+        pyfbu.nBurn            = 1000
+        pyfbu.nThin            = 50
+        pyfbu.run()
