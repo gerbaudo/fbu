cf63373e2931abbe43c96c575bfd9b774fa0fdfd
francescorubbo <rubbo.francesco@gmail.com>
2013-12-09 21:47:59 +0100

    now changing setup...before i was actually deleting the README.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1dev.7',
     packages=['fbu',],
     license='LICENSE.txt',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst','rt').read(),
     url='https://pypi.python.org/pypi/fbu',
     author='Davide Gerbaudo,Clement Helsens,Francesco Rubbo',
     author_email='rubbo.francesco@gmail.com',
