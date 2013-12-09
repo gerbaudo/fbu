e747d7e53dde62e98149389a998d8f0b1c419355
francescorubbo <rubbo.francesco@gmail.com>
2013-12-09 22:32:32 +0100

    expand description in README and setup.py

--- README.rst
+++ README.rst
@@ -1,16 +1,23 @@
 .. image:: https://travis-ci.org/gerbaudo/fbu.png
    :target: https://travis-ci.org/gerbaudo/fbu
 
-========================
-Fully Bayesian Unfolding
-========================
+=====
+PyFBU
+=====
 
-Implementation of the FBU algorithm described in
+Implementation of the Fully Bayesian Unfolding algorithm described in
 `physics.data-an/1201.4612 <http://arxiv.org/abs/1201.4612>`_.
 
-Contributors
+Installation
 ------------
-davide.gerbaudo@cern.ch
-clement.helsens@cern.ch
-francesco.rubbo@cern.ch
 
+The latest version of PyFBU can be installed using pip:
+
+```
+pip install --pre fbu
+```
+
+Dependencies
+------------
+
+PyFBU is tested on Python 2.7 and depends on PyMC, Numpy and Matplotlib.
--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 from distutils.core import setup
 
+DISTNAME = 'fbu'
+DESCRIPTION = "PyFBU"
+VERSION = '0.0.1dev.7'
+AUTHOR = 'Davide Gerbaudo, Clement Helsens and Francesco Rubbo'
+AUTHOR_EMAIL = 'rubbo.francesco@gmail.com'
+URL = 'https://github.com/gerbaudo/fbu'
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Operating System :: OS Independent',
+    ]
+
+required = ['pymc==2.3','numpy>=1.8.0','matplotlib>=1.2.1']
+
 setup(
-    name='fbu',
-    version='0.0.1dev.7',
+    name=DISTNAME,
+    version=VERSION,
     packages=['fbu',],
     license='LICENSE.txt',
     long_description=open('README.rst','rt').read(),
-    url='https://pypi.python.org/pypi/fbu',
-    author='Davide Gerbaudo,Clement Helsens,Francesco Rubbo',
-    author_email='rubbo.francesco@gmail.com',
+    url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    classifiers=classifiers,
+    install_requires=required,
     )
     
 ##to upload package
