e6e5509e68a9260f6c215451b981d7b8fdd294ce
francescorubbo <rubbo.francesco@gmail.com>
2013-12-18 18:00:20 +0100

    add __version__ and import numpy

--- fbu/__init__.py
+++ fbu/__init__.py
@@ -1,1 +1,9 @@
+__version__ = '0.0.1'
+
+try:
+    import numpy
+except ImportError:
+    raise ImportError(
+        'NumPy does not seem to be installed. Please see the installation guide.')
+
 from .PyFBU import PyFBU
