693fc279dcd283ce94b24881a48c5ff83691aa9e
francescorubbo <rubbo.francesco@gmail.com>
2013-12-20 17:00:32 +0100

    adding tests area

---
+++ fbu/tests/__init__.py
@@ -0,0 +1,16 @@
+import warnings
+
+try:
+    from numpy.testing import Tester
+
+    with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            test = Tester().test
+
+except ImportError:
+    warnings.warn(
+        'NumPy 1.2 and nose are required to run the test suite.',
+        ImportWarning)
+
+    def test():
+        return "Please install nose to run the test suite."
