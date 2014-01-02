27cd35044c034543ba2d0a0398136e98ccab62d1
francescorubbo <rubbo.francesco@gmail.com>
2014-01-02 12:51:16 +0100

    example of reco/truth different binning

--- bin/runpyFBU.py
+++ bin/runpyFBU.py
@@ -14,15 +14,15 @@ if __name__ == "__main__":
     pyfbu.nBurn            = 20000
     pyfbu.nThin            = 1
 #    pyfbu.potential = 'Tikhonov'
-    pyfbu.data             = [100,150]
-    pyfbu.response         = [[0.08,0.02], #first truth bin
-                              [0.02,0.08]] #second truth bin
+    pyfbu.data             = [20,100,150,20]
+    pyfbu.response         = [[0.01,0.06,0.02,0.01], #first truth bin
+                              [0.01,0.02,0.06,0.01]] #second truth bin
     ndim = len(pyfbu.response)
     pyfbu.lower            = [0]*ndim
     pyfbu.upper            = [3000]*ndim
-    pyfbu.background       = {'bckg': [10,40]}
+    pyfbu.background       = {'bckg': [5,10,40,5]}
     pyfbu.backgroundsyst   = {'bckg': 0.}
-    pyfbu.objsyst = {'jes':[0.05,0.03]}
+#    pyfbu.objsyst = {'jes':[0.05,0.03]}
 #    pyfbu.systfixsigma = -1.
     pyfbu.monitoring = True
     pyfbu.name = 'test'
