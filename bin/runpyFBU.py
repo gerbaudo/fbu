a5877bfe10d9ee12bed4da65e26a4e5a9b72407e
francescorubbo <rubbo.francesco@gmail.com>
2014-01-02 11:57:40 +0100

    simplify example script

--- bin/runpyFBU.py
+++ bin/runpyFBU.py
@@ -8,58 +8,22 @@ from optparse import OptionParser
 
 #__________________________________________________________
 if __name__ == "__main__":
-    projectdir = os.path.dirname(os.path.abspath(__file__)).replace('/bin','/')
-    datadir = projectdir+'tests/linearity/data/mc11/'
-    defaultdata = datadir+'dataA2pos.json'
-    defaultmig  = datadir+'migrations.json'
-    defaultbkg  = datadir+'background.json'
-    usage = "usage: %prog -t python/unfold_template.py [options]"
-    parser = OptionParser(usage=usage)
-    parser.add_option ('-D', '--data', default=defaultdata, help="json data file")
-    parser.add_option ('-M', '--matrix', default=defaultmig, help="json migration matrix file")
-    parser.add_option ('-B', '--background', default=defaultbkg, help="json background file")
-    parser.add_option ('-s', '--seed', default=-1, help='random seed value. should be greater than -1 to ues a different random seed. Default is -1, so same random seed')
-    parser.add_option ('-v', '--verbose', help='Toggle verbose', action='store_true', default=False)
-    (opts, args) = parser.parse_args()
-    jsondata = opts.data
-    jsonmig = opts.matrix
-    jsonbkg = opts.background
-    rndseed = opts.seed
-    verbose = opts.verbose
 
     pyfbu = PyFBU()
     pyfbu.nMCMC            = 100000
     pyfbu.nBurn            = 20000
     pyfbu.nThin            = 1
 #    pyfbu.potential = 'Tikhonov'
-    import json
-    pyfbu.data             = json.load(open(jsondata))
-    ndim = len(pyfbu.data)
-    pyfbu.lower            = [20000]*ndim
-    pyfbu.upper            = [180000]*ndim
-    pyfbu.response         = json.load(open(jsonmig))
-    pyfbu.background       = json.load(open(jsonbkg))
-    pyfbu.backgroundsyst = {'bckg':0.}
-#    pyfbu.objsyst = {'jes':[0.05,0.03,0.03,0.05]}
+    pyfbu.data             = [100,150]
+    pyfbu.response         = [[0.08,0.02], #first truth bin
+                              [0.02,0.08]] #second truth bin
+    ndim = len(pyfbu.response)
+    pyfbu.lower            = [0]*ndim
+    pyfbu.upper            = [3000]*ndim
+    pyfbu.background       = {'bckg': [10,40]}
+    pyfbu.backgroundsyst   = {'bckg': 0.}
+    pyfbu.objsyst = {'jes':[0.05,0.03]}
 #    pyfbu.systfixsigma = -1.
-    pyfbu.rndseed          = int(rndseed)
-    pyfbu.verbose          = verbose
     pyfbu.monitoring = True
     pyfbu.name = 'test'
     pyfbu.run()
-
-    if False:
-        trace = pyfbu.trace
-        from numpy import mean,std
-        mm = mean(trace,1)
-        ss = std(trace,1)
-        
-        pyfbu.lower            = mm-5*ss
-        pyfbu.upper            = mm+5*ss
-        
-        pyfbu.name = 'optim'
-        pyfbu.backgroundsyst = {'bckg':0.1}
-        pyfbu.nMCMC            = 500000
-        pyfbu.nBurn            = 1000
-        pyfbu.nThin            = 50
-        pyfbu.run()
