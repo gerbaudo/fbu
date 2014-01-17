43312fc42c9f23e55f662b524dbde93b5784b252
francescorubbo <rubbo.francesco@gmail.com>
2014-01-17 11:49:22 +0100

    create Regularization class

--- fbu/potentials.py
+++ fbu/potentials.py
@@ -1,4 +1,4 @@
-import pymc
+from pymc import Potential
 
 def dummy(**args): return 0. 
 from tikhonov import tikhonov
@@ -7,13 +7,22 @@ potentialdict = {
     'Tikhonov':tikhonov,
     }
 
-def wrapper(potname='',truth=None,size=1,other_args={}):
+class Regularization(object):
+    def __init__(self,regname='',size=1,other_args={}):
+        self.regname = regname
+        self.size = size
+        self.other_args = other_args
+        self.function = dummy
+        if regname in potentialdict: 
+            self.function = potentialdict[regname]
+        else:
+            print 'WARNING: potential name not found! Falling back to no potential...'
 
-    potential = 0.
-    if potname in potentialdict: 
-        potmethod = potentialdict[potname] 
-        default_args = dict(value=truth,size=size)
-        args = dict(default_args.items()+other_args.items())
-        potential = potmethod(**args)
-
-    return potential
+    def wrapper(self,truth=None):
+        default_args = dict(value=truth,size=self.size)
+        args = dict(default_args.items()+self.other_args.items())
+        potential = self.function(**args)
+        return potential
+    
+    def getpotential(self,truth):
+        return Potential(self.wrapper,self.regname,self.regname,{'truth':truth})
