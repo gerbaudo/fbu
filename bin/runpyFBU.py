b081d1a88c4516367508cb0e9bcf053f3af14d9f
francescorubbo <frubbo@Francescos-MacBook-Pro.local>
2014-08-02 15:40:24 +0200

    update test script

--- bin/runpyFBU.py
+++ bin/runpyFBU.py
@@ -1,48 +1,75 @@
 #!/bin/env python
 
-import os
 import sys
 sys.path.append('fbu')
 from PyFBU import PyFBU
-from optparse import OptionParser
 
 #__________________________________________________________
 if __name__ == "__main__":
 
     pyfbu = PyFBU()
-    pyfbu.nMCMC            = 100000
-    pyfbu.nBurn            = 20000
-    pyfbu.nThin            = 1
-    pyfbu.data             = [20,100,150,20]
-    pyfbu.response         = [[0.06,0.02,0.01,0.01], #first truth bin
-                              [0.02,0.06,0.02,0.01],
-                              [0.01,0.02,0.06,0.02], #first truth bin
-                              [0.01,0.01,0.02,0.06]] #second truth bin
+    pyfbu.data             = [100,150]
+    pyfbu.response         = [[0.08,0.02], #first truth bin
+                              [0.02,0.08]]
     ndim = len(pyfbu.response)
-    pyfbu.lower            = [40,200,300,40]
-    pyfbu.upper            = [800,2000,3000,800]
+    pyfbu.lower            = [500,500]
+    pyfbu.upper            = [3000,3000]
 
-    from Regularization import Regularization
-    pyfbu.regularization = Regularization('Tikhonov',parameters=[{'refcurv':0.,'alpha':0.1},{'refcurv':0.,'alpha':0.1}])
+    pyfbu.monitoring = True
+    pyfbu.name = 'test'
 
-    pyfbu.background       = {'bckg1': [5,5,20,5],'bckg2': [0,5,20,0]}
-    pyfbu.backgroundsyst   = {'bckg1': 0.,'bckg2': 0.}
+    pyfbu.background       = {'bckg1': [5,20],'bckg2': [5,30]}
+    pyfbu.backgroundsyst   = {'bckg1': 0.5,'bckg2': 0.4}
+
     pyfbu.objsyst = { 
-        'signal':{'jes':[0.05,0.05,0.05,0.05],
-                  'jer':[0.03,0.03,0.03,0.03]
-                  },
+        'signal':{
+            's1':[0.05,0.05],
+            's2':[0.03,0.03],
+            's3':[0.03,0.03],
+            's4':[0.03,0.03],
+            's5':[0.03,0.03],
+            's6':[0.03,0.03],
+            's7':[0.03,0.03],
+            's8':[0.03,0.03],
+            },
         'background':{
-            'jes':{
-                'bckg1':[0.05,0.05,0.05,0.05],
-                'bckg2':[0.01,0.01,0.01,0.01]
+            's1':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
                 },
-            'jer':{
-                'bckg1':[0.01,0.01,0.01,0.01],
-                'bckg2':[0.05,0.05,0.05,0.05]
+            's2':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
+                },
+            's3':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
+                },
+            's4':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
+                },
+            's5':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
+                },
+            's6':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
+                },
+            's7':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
+                },
+            's8':{ 
+                'bckg1': [0.05,0.05],
+                'bckg2': [0.05,0.05]
                 }
             }
         }
-    pyfbu.systfixsigma = -1.
-    pyfbu.monitoring = True
-    pyfbu.name = 'test'
+
     pyfbu.run()
+
+    from numpy import mean, std
+    for bin in pyfbu.trace:
+        print mean(bin),std(bin)
