bad795595150eb13c2420a629b14e6cb3d85a7b1
francescorubbo <rubbo.francesco@gmail.com>
2018-06-16 14:05:13 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 DISTNAME = 'fbu'
 DESCRIPTION = "PyFBU"
 #VERSION = '0.0.2'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 AUTHOR = 'Davide Gerbaudo, Clement Helsens and Francesco Rubbo'
 AUTHOR_EMAIL = 'rubbo.francesco@gmail.com'
 URL = 'https://github.com/gerbaudo/fbu'
